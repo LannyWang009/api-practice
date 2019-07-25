4bc990c81ad0bfe6ee4e702f3ef680c7ee9b5409
Lanzhen Wang <lanzhenwang@Lanzhens-MacBook-Pro.local>
2019-07-24 21:58:20 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api ={
+  link: 'https://api.nasa.gov/planetary/apod?',
+  api_key: 't1OrOzDZ7OKC4gVNioVI2an00u2g5oReVWz2qifH',
+  count: 5
 }
 
-export default App;
+const getData = async () =>  {
+  let useData = [];
+  try {
+    const res = await axios.get(
+      ""+ api.link + "api_key=" + api.api_key + "&count=" + api.count
+    )
+    
+    // console.info('res.data', res.data)
+    useData = res.data
+    // console.info('useData', useData) // array of titles
+    return useData
+    // setData(useData)
+  } 
+   catch (err) {
+     console.error(err)
+  }
+}
+
+// uplift
+// move getData outside the async into useEffect to make functions more pure
+
+export default function App() {
+  const [data, setData] = useState([])
+  useEffect(()=> {
+    getData()
+    .then((data)=>setData(data)
+    )}, [])
+  // telling to run it the first it mounts
+
+  
+  return (
+    <div>
+      {data.map((element) => (<div key={element.mal_id}> {element.title} </div>))}
+    </div>
+  )
+}
